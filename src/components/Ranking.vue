16ce0ebc1b9791afa44601c8e7e797261bf8936b
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-18 01:50:39 +0900

    画像追加

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -3,7 +3,7 @@
     <h2>推しメン決定！</h2>
     <ol>
       <li v-for="member in members" :key="member.id">
-        {{ member.name }} (勝ち点: {{ member.score }})
+        {{ member.name }} <img :src="member.image" :alt="member.name" />(スコア: {{ member.score }})
       </li>
     </ol>
   </div>
