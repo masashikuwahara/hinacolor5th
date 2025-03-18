a504a59e0f67e7d39d34f6d8c8f5a17e4fda823d
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-19 01:14:00 +0900

    トップページ実装

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <h1>日向坂5期生推しメンチェッカー</h1>
+    <p>あなたの推しメンを決めよう！</p>
+    <router-link to="/battle">
+      <button class="start-button">推すしかねえので</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+.start-button {
+  padding: 15px 30px;
+  font-size: 18px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.start-button:hover {
+  background-color: #0056b3;
+}
+</style>
