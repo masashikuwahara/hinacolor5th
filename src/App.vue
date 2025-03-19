121783b66d4875b6637d6a79b31abfbb3b8ad077
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-20 01:07:22 +0900

    画面遷移時のアニメーション追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view />
+  <div id="app">
+    <Transition name="fade">
+      <RouterView />
+    </Transition>
+  </div>
 </template>
 
 <script>
@@ -9,3 +13,13 @@ export default defineComponent({
   name: "App"
 });
 </script>
+
+<style>
+/* ページ遷移のアニメーション */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease-in-out;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
